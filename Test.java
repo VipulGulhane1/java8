4edda057efc64e30c208a810b1e50c18d9fd2ae4
VipulGulhane1 <36219834+VipulGulhane1@users.noreply.github.com>
2019-10-13 12:23:56 +0530

    Update Test.java
    http://mytechnologythought.blogspot.com/
    I have a String[] with values like so:
    public static final String[] VALUES = new String[] {"AB","BC","CD","AE"};
    Given String s, is there a good way of testing whether VALUES contains s?
    ANS:
    Java 8 predicate test method is best for it.
    test(T t) : Evaluates this predicate on the given argument.boolean test(T t)
    test(T t) Parameters: t - the input argument Returns: true if the input argument matches the predicate, otherwise false
    Here is a full example of it.

--- Test.java
+++ Test.java
@@ -3,7 +3,6 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class Test {
-
  public static final List<String> VALUES = Arrays.asList("AA", "AB", "BC", "CD", "AE");
 
  public static void main(String args[]) {
